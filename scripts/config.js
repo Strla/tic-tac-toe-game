9ebdd39b641fe3d66ed50318ee1826d136c0fa01
Ivan Starčević <ivan.starcevic15@gmail.com>
2021-12-16 15:54:19 +0100

    Handle modal click events

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,9 @@
-function openPlayerConfig() {}
+const openPlayerConfig = () => {
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+};
+
+const closePlayerConfig = () => {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+};
